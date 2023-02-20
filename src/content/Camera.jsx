9ae4ada503e77d77a55493d3a8c65c00f7d51973
Prajwal Upadhya <prajwal.19ec035@sode-edu.in>
2023-02-20 12:41:02 +0530

    . removed

--- src/content/Camera.jsx
+++ src/content/Camera.jsx
@@ -4,7 +4,7 @@ import CaptureImage from './CaptureImage'
 function Camera() {
 	return (
 		<div>
-			<h2>Capture the image of the Accident and We will send it nearest hospital and police station.</h2>
+			<h2>Capture the image of the Accident and We will send it nearest hospital and police station</h2>
 			<CaptureImage></CaptureImage>
 		</div>
 	)
